5e18ab856d513ccf01586c4cff3d9708084d8412
Marcio Gabriel Holm Monteiro <Marci.Holm@MacBook-Air-de-Marcio-Holm.local>
2026-02-21 03:05:45 -0300

    fix: explicit port binding to 0.0.0.0 and sync db region to us-west-2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const port = process.env.PORT || 3001;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
